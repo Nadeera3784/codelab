aecaa3d6ccb2dd52287c62ea7ef74a8837bc0ac4
dev-PC\dev <nadeera3784@gmail.com>
2021-05-23 13:59:29 +0530

    feat :   Invoice Generator added

--- src/actions/PostActions.js
+++ src/actions/PostActions.js
@@ -11,41 +11,14 @@ const Store_posts = [
     post_url : 'https://angular-shopping-cart.netlify.app'
   },
   {
-    _id : '9334834343',
-    category_title: 'angular',
-    post_title: 'macbook air 2018 review',
-    post_slug: 'macbook_air_2018_review',
-    post_description: 'in ornare urna vitae libero blandit, vel hendrerit metus pharetra ligula.',
-    post_create_date: '2020-07-01T14:12:42.485Z',
-    post_url : 'www.google.com'
-  },
-  {
-    _id : '09734734734',
-    category_title: 'react',
-    post_title: 'airpods are cool',
-    post_slug: 'airpods_are_cool',
-    post_description: 'in ornare urna vitae libero blandit, vel hendrerit metus pharetra ligula.',
-    post_create_date: '2020-07-01T14:13:09.032Z',
-    post_url : 'www.google.com'
-  },
-  {
     _id : '347387384834',
     category_title: 'react',
-    post_title: 'react.js conferences to attend in 2019',
-    post_slug: 'react.js_conferences_to_attend_in_2019',
-    post_description: 'conferences are a perfect way to make friends and learn from experts. here are some react conferences to look forward to in 2019.',
-    post_create_date: '2020-07-01T14:14:49.987Z',
-    post_url : 'www.google.com'
+    post_title: 'Invoice Generator',
+    post_slug: 'react-invoice-generator',
+    post_description: 'React invoice generator app build with reactjs',
+    post_create_date: '2021-03-03T14:14:49.987Z',
+    post_url : 'https://react-invoice-generator.netlify.app/'
   },
-  {
-    _id : '2236526352632',
-    category_title: 'react',
-    post_title: 'making your site private',
-    post_slug: 'making_your_site_private',
-    post_description: 'sometimes you might want to put your site behind closed doors. if you\'ve got a publication that you don\'t want the world',
-    post_create_date: '2020-07-01T14:15:37.979Z',
-    post_url : 'www.google.com'
-  }
 ];
 
 export function getPosts() {
